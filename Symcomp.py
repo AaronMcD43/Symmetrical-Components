bb73cbc029f8d6bad6faf56d5e0d5a2446d138f6
Aaron McDonnell <55592619+AaronMcD43@users.noreply.github.com>
2020-05-05 18:58:31 +0100

    Add files via upload
    The program calculates the positive negative and zero sequence components from an unbalanced set of phasors. In this version I have changed from an arrow plot to a quiver plot. The sequence components are plotted on a graph.

--- Symcomp.py
+++ Symcomp.py
@@ -3,35 +3,21 @@ import numpy as np
 import math
 import cmath
 
-origin = 0
+phaseA = [1, 23]
+phaseB = [1.32, 120]
+phaseC = [1.56, -121]
 
 
-class phase:
-
-    def __init__(self, mag, ang):
-        self.mag = mag
-        self.ang = ang
-
-    def deg_car(self):
-        self.mag = self.mag * math.cos(self.ang)
-        self.ang = self.mag * math.sin(self.ang)
+def deg_car(mag, ang):
+    x = mag * math.cos(math.radians(ang))
+    y = mag * math.sin(math.radians(ang))
+    return(complex(x, y))
 
 
-phaseA = phase(1, 0)
-phaseB = phase(1, 120)
-phaseC = phase(1, 240)
+phaseAcar = deg_car(phaseA[0], phaseA[1])
+phaseBcar = deg_car(phaseB[0], phaseB[1])
+phaseCcar = deg_car(phaseC[0], phaseC[1])
 
-print(phaseA.mag)
-print(phaseA.ang)
-
-# phaseA.deg_car() #calls the method in the class that convertes from polar to rectangular
-
-# print(phaseA.mag) #prints the polar to rectangular after the method is called
-# print(phaseA.ang) #prints the polar to rectangular after the method is called
-
-
-# print(phaseB)
-# print(phaseC)
 
 # a operator 1<120
 a = complex(-0.5+0.866j)
@@ -40,36 +26,29 @@ A = np.array([[1, 1, 1],
               [1, a**2, a],
               [1, a, a**2]], dtype=complex)
 
-# print(A)
-
-Ainv = np.linalg.inv(A)  # calculates the inverse of a matrix
+Ainv = np.linalg.inv(A)  # calculates the inverse of the A matrix
 # creates a 1x3 matrix
-sys = np.array([[1.13+1.2j], [0.52-1.22j], [-0.803-1.33j]])
-sym = Ainv.dot(sys)
-# print(sym[1])
-# print(cmath.polar(sym[1]))
+sys = np.array([[deg_car(phaseA[0], phaseA[1])],
+                [deg_car(phaseB[0], phaseB[1])],
+                [deg_car(phaseC[0], phaseC[1])]])
+sym = Ainv.dot(sys)  # calculated symmetrical components
+
+plt.quiver(0, 0, phaseAcar.real, phaseAcar.imag, angles='xy',
+           scale_units='xy', scale=1, width=0.005, color="red")
+plt.quiver(0, 0, phaseBcar.real, phaseBcar.imag, angles='xy',
+           scale_units='xy', scale=1, color="yellow")
+plt.quiver(0, 0, phaseCcar.real, phaseCcar.imag, angles='xy',
+           scale_units='xy', scale=1, color="blue")
 
 
-# def car_pol(x, y):  # x is the axis and y is the y axis
-#     cn = complex(x, y)
-#     a = abs(cn)
-#     b = np.angle(cn, deg=True)
+plt.quiver(0, 0, sym[1].real, sym[1].imag, angles='xy',
+           scale_units='xy', scale=1, width=0.005)
+plt.quiver(sym[1].real, sym[1].imag, sym[2].real, sym[2].imag, angles='xy',
+           scale_units='xy', scale=1, width=0.005)
+plt.quiver(sym[2].real+sym[1].real, sym[2].imag+sym[1].imag, sym[0].real, sym[0].imag, angles='xy',
+           scale_units='xy', scale=1, width=0.005)
 
-#     return(a, b)
+plt.xlim(-2, 2)
+plt.ylim(-2, 2)
 
-ax = plt.subplot(111, projection='polar')
-plt.arrow(math.radians(phaseA.ang), origin, 0, phaseA.mag, width=0.015,
-          edgecolor='red', lw=2, zorder=5)
-plt.arrow(math.radians(phaseB.ang), origin, 0, phaseB.mag, width=0.015,
-          edgecolor='yellow', lw=2, zorder=5)
-plt.arrow(math.radians(phaseC.ang), origin, 0, phaseC.mag, width=0.015,
-          edgecolor='blue', lw=2, zorder=5)
-
-
-ax.set_rmax(2)
-ax.set_rticks([0.5, 1, 1.5, 2])  # less radial ticks
-ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
-ax.grid(True)
-
-ax.set_title("Symmetrical Components", va='bottom')
 plt.show()
