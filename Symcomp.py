1987bbd7dbb5e4f6c9d802abd7ac60c54e6d286a
Aaron McDonnell <55592619+AaronMcD43@users.noreply.github.com>
2020-05-08 07:44:44 +0100

    Add files via upload
    Some general tidying and improvement of comment descriptions

--- Symcomp.py
+++ Symcomp.py
@@ -3,12 +3,13 @@ import numpy as np
 import math
 import cmath
 
-phaseA = [1, 23]
-phaseB = [1.32, 120]
+#Input Values
+phaseA = [1.65, 46.8]
+phaseB = [1.32, -67]
 phaseC = [1.56, -121]
 
-
-def deg_car(mag, ang):
+#Function takes two arguments as magnitude and angle and converts them to a complex number
+def deg_car(mag, ang):                    
     x = mag * math.cos(math.radians(ang))
     y = mag * math.sin(math.radians(ang))
     return(complex(x, y))
@@ -21,18 +22,24 @@ phaseCcar = deg_car(phaseC[0], phaseC[1])
 
 # a operator 1<120
 a = complex(-0.5+0.866j)
-# Symmetrical Components Matrix
+a2= a*a
+# Symmetrical Components A Matrix
 A = np.array([[1, 1, 1],
               [1, a**2, a],
               [1, a, a**2]], dtype=complex)
 
-Ainv = np.linalg.inv(A)  # calculates the inverse of the A matrix
-# creates a 1x3 matrix
+# calculates the inverse of the A matrix
+Ainv = np.linalg.inv(A)  
+
+# creates a 3x1 matrix
 sys = np.array([[deg_car(phaseA[0], phaseA[1])],
                 [deg_car(phaseB[0], phaseB[1])],
                 [deg_car(phaseC[0], phaseC[1])]])
-sym = Ainv.dot(sys)  # calculated symmetrical components
 
+# calculated symmetrical components
+sym = Ainv.dot(sys)  
+
+# Plotting phase A, B & C
 plt.quiver(0, 0, phaseAcar.real, phaseAcar.imag, angles='xy',
            scale_units='xy', scale=1, width=0.005, color="red")
 plt.quiver(0, 0, phaseBcar.real, phaseBcar.imag, angles='xy',
@@ -40,15 +47,20 @@ plt.quiver(0, 0, phaseBcar.real, phaseBcar.imag, angles='xy',
 plt.quiver(0, 0, phaseCcar.real, phaseCcar.imag, angles='xy',
            scale_units='xy', scale=1, color="blue")
 
-
+#Plotting the positve, negative and zero sequence components for phase A
 plt.quiver(0, 0, sym[1].real, sym[1].imag, angles='xy',
-           scale_units='xy', scale=1, width=0.005)
+           scale_units='xy', scale=1, width=0.003)
 plt.quiver(sym[1].real, sym[1].imag, sym[2].real, sym[2].imag, angles='xy',
-           scale_units='xy', scale=1, width=0.005)
+           scale_units='xy', scale=1, width=0.003)
 plt.quiver(sym[2].real+sym[1].real, sym[2].imag+sym[1].imag, sym[0].real, sym[0].imag, angles='xy',
-           scale_units='xy', scale=1, width=0.005)
+           scale_units='xy', scale=1, width=0.003)
+
 
 plt.xlim(-2, 2)
 plt.ylim(-2, 2)
 
 plt.show()
+
+print(sym[0])
+print(sym[1])
+print(sym[2])
